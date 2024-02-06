4c0e9e6b7c5a60bf79f9f7c7b38e80cd8ec1ec3c
HiVowelHam <159086811+HiVowelHam@users.noreply.github.com>
2024-02-05 22:24:30 -0300

    Update product.py
    Adicionando umas partes que não estavam aqui pq eu dei upload do arquivo estando esse desatualizado pois eu ainda não tinha salvado

--- Models/product.py
+++ Models/product.py
@@ -1,5 +1,5 @@
 import date
-from user.py import usuario.id
+from /Models/user.py import usuario.id
 
 class produto:
     def __init__ (self, nome: str, descriçao: str, add_since: date.today, estado_uso: bool = True, region: str, _quantidade: int):
@@ -7,4 +7,9 @@ class produto:
         self.descriçao = descriçao;
         self.add_since = add_since;
         self.usuario_id = usuario.id
-        
+        self.estado_uso = estado_uso;
+        self.region = region;
+        self._quantidade = _quantidade;
+        self.compras = compras;
+        self.avaliaçoes = avaliaçoes;
+        self.comentarios = comentarios
