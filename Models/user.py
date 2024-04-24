55fdfe45892e29dd65162ce4c50209ea8aa866f9
HiVowelHam <159086811+HiVowelHam@users.noreply.github.com>
2024-04-23 22:14:16 -0300

    Delete Models directory

--- Models/user.py
+++
@@ -1,9 +0,0 @@
-class usuario:
-    
-    def __init__ (self, nome: str, idade: int, id: str, dinheiro: float) :
-        self.nome = nome;
-        self.idade = idade;
-        self.id = id;
-        self.dinheiro = dinheiro;
-        
-           
